3ca213c1f3699a63444179650abd677718c8a422
Teng Keng Khoo <30566073+TengKengK@users.noreply.github.com>
2018-05-04 14:23:19 +0400

    2nd Commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+
+	var scrollLink = $('.scroll');
+
+	//smooth scrolling
+	scrollLink.click(function(e) {
+		e.preventDefault();
+		$('body,html').animate({
+			 scrollTop: $(this.hash).offset().top
+		}, 1000);
+	});
+})
